7b780f1dd52dd9279af909f248b095cfa12621a1
Mara <Zainullin.marat93@yandex.ru>
2024-09-24 18:49:54 +0300

    14.2

--- src/category.py
+++ src/category.py
@@ -1,3 +1,6 @@
+from src.product import Product
+
+
 class Category:
     name: str
     description: str
@@ -8,7 +11,28 @@ class Category:
     def __init__(self, name, description, products):
         self.name = name
         self.description = description
-        self.list_product = products
+        self.__list_product = products
         Category.category_count += 1
         Category.product_count += len(products)
 
+    def add_product(self, product: Product):
+        self.__list_product.append(product)
+        Category.product_count += 1
+
+    @property
+    def products(self):
+        product_str = ''
+        for product in self.__list_product:
+            product_str += f'\n{product.name}, {product.price} руб. Остаток: {product.quantity} шт.'
+        return product_str
+
+    @property
+    def products_in_list(self):
+        return self.__list_product
+        for product in self.__list_product:
+            product_str += f'\n{product.name}, {product.price} руб. Остаток: {product.quantity} шт.'
+        return product_str
+
+    @property
+    def products_in_list(self):
+        return self.__list_product
